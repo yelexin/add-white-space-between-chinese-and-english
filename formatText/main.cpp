308153b1c9d07bafd6b4af38044b6f50d10e28cd
Mark Yeh <yelexin@hotmail.com>
2019-02-07 16:55:45 +0800

    Initial Commit

---
+++ formatText/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  formatText
+//
+//  Created by mark on 2019/2/7.
+//  Copyright © 2019 mark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
